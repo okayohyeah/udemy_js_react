c55a1ae5a30d0337d82d0bdc56a0383fcb5efc8c
Katherine Oh <ohkathy@gmail.com>
2017-11-27 17:34:47 -0800

    Events and Callbacks Exercise

---
+++ section3.13/toggle.js
@@ -0,0 +1,5 @@
+document.getElementById('theme').addEventListener('click', function() {
+	// toggles between 'theme2' class
+	// classList property returns the class name(s) of an elements, body for this exercise 
+	document.body.classList.toggle('theme2');
+});
