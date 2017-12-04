fd42c97e33648a706e8671b467d7d8ea58bcbec0
Katherine Oh <ohkathy@gmail.com>
2017-12-03 21:01:56 -0800

    section 5.22: JSON activity

--- section5.22/jsonhandling.js
+++ section5.22/jsonhandling.js
@@ -5,31 +5,40 @@ function getUsername() {
 		return response_obj.json();
 	})
 	.then(function(obj) {
+
+		// check object returned
 		console.log(obj);
-		document.getElementById("username").innerText = "Username: " + obj.login;
+		if (obj.name !== null) {
+			document.getElementById("name").innerText = obj.name;
+		} else {
+			document.getElementById("name").innerText = "A user has no name";
+		}
+		
+		document.getElementById("username").innerText = obj.login;
 		document.getElementById("pic").src = obj.avatar_url;
-		document.getElementById("name").innerText = "Name: " +obj.name;
+
+		document.getElementById("fill-icon").setAttribute("d", "M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z");
 
 		if (obj.location !== null) {
-			document.getElementById("location").innerText = "Lives in: " + obj.location;
+			document.getElementById("location").innerText = obj.location;
 		} else {
-			document.getElementById("location").innerText = "Lives in space";
+			document.getElementById("location").innerText =  "Space";
 		}
-
+		
 		if (obj.followers > 1) {
-			document.getElementById("followers").innerText = obj.followers + " followers.";
+			document.getElementById("followers").innerText = obj.followers + " followers";
 		} else if (obj.followers == 1) {
-			document.getElementById("followers").innerText = obj.followers + " follower.";
+			document.getElementById("followers").innerText = obj.followers + " follower";
 		} else {
 			document.getElementById("followers").innerText = "No followers";
 		}
 		
 		if (obj.following > 1) {
-			document.getElementById("following").innerText = "Following " + obj.following + " users.";
+			document.getElementById("following").innerText = "Following " + obj.following + " users";
 		} else if (obj.following == 1) {
-			document.getElementById("following").innerText = "Following " + obj.following + " user.";
+			document.getElementById("following").innerText = "Following " + obj.following + " user";
 		} else {
-			document.getElementById("followers").innerText = "Not following any users.";
+			document.getElementById("followers").innerText = "Not following any users";
 		}
 
 		getFollowers(obj.followers_url);
@@ -48,6 +57,8 @@ function getFollowers(url) {
 		return followers_obj.json();
 	})
 	.then(function(obj) {
+
+		//check obj returned
 		console.log(obj);
 	})
 }
@@ -61,6 +72,7 @@ function showFollowers(url) {
 		obj.forEach(function(item,index) {
 				var avatar = document.createElement("img");
 				avatar.setAttribute("src", obj[index].avatar_url);
+				avatar.setAttribute("id", "f-icon");
 				avatar.setAttribute("width", "6%");
 				avatar.setAttribute("height", "6%");
 				avatar.setAttribute("alt", "Avatar");
@@ -76,6 +88,8 @@ function getFollowing(url) {
 		return following_obj.json();
 	})
 	.then(function(obj) {
+
+		// check for obj returned
 		console.log(obj);
 	})
 }
@@ -83,12 +97,14 @@ function getFollowing(url) {
 function showFollowing(url) {
 	fetch(url)
 	.then(function(show_obj) {
+		// check for obj returned
 		return show_obj.json();
 	})
 	.then(function(obj) {
 		obj.forEach(function(item,index) {
 				var avatar = document.createElement("img");
 				avatar.setAttribute("src", obj[index].avatar_url);
+				avatar.setAttribute("id", "f-icon");
 				avatar.setAttribute("width", "6%");
 				avatar.setAttribute("height", "6%");
 				avatar.setAttribute("alt", "Avatar");
