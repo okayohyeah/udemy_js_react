391988a47e3facb1843ba35246ccaa1233bfb696
Katherine Oh <ohkathy@gmail.com>
2017-12-22 08:29:28 -0800

    WIP: framework and JS

---
+++ section7.33/index.js
@@ -0,0 +1,22 @@
+function getUserChoice() {
+	var rover = document.getElementById("user_rover_name").value;
+	var camera = document.getElementById("user_camera_name").value;
+	var sol = document.getElementById("user_sol").value;
+
+	fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?sol=" + sol + "&camera=" + camera + "&api_key=DEMO_KEY")
+	.then(response => {
+			if (response.status === 200) {
+				return response.json();
+			} else {
+				var error = new Error(response.statusText || response.status);
+				error.response = response;
+				return reject(error);
+			}
+	})
+	.then( obj => {
+		// check object returned
+		console.log(obj);
+		// TODO: DO something with object
+	})
+}
+
