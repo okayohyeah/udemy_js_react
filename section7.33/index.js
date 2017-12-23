8ea16b1bf1ec4c0b2c307c76fe1dd865a478c184
Katherine Oh <ohkathy@gmail.com>
2017-12-23 11:09:56 -0800

    WIP: JS

--- section7.33/index.js
+++ section7.33/index.js
@@ -3,14 +3,14 @@ function getUserChoice() {
 	var camera = document.getElementById("user_camera_name").value;
 	var sol = document.getElementById("user_sol").value;
 
-	fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?sol=" + sol + "&camera=" + camera + "&api_key=DEMO_KEY")
+	fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?sol=" + sol + "&camera=" + camera + "&api_key=DEMO_KE")
 	.then(response => {
 			if (response.status === 200) {
 				return response.json();
 			} else {
 				var error = new Error(response.statusText || response.status);
 				error.response = response;
-				return reject(error);
+				throw error;
 			}
 	})
 	.then( obj => {
