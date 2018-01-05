e7816492088e2ecd80d351134e413fed69f20e36
Katherine Oh <ohkathy@gmail.com>
2018-01-04 20:56:36 -0800

    populate second dropdown list from selection of first dropdown list

--- section7.33/index.js
+++ section7.33/index.js
@@ -1,7 +1,45 @@
+var rovers_by_cameras = {
+	curiosity: [
+		{name: "fhaz", full_name: "Front Hazard Avoidance Camera"},
+		{name: "rhaz", full_name: "Rear Hazard Avoidance Camera"},
+		{name: "mast", full_name: "Mast Camera"},
+		{name: "chemcam", full_name: "Chemistry and Camera Complex"},
+		{name: "mahli", full_name: "Mars Hand Lens Imager"},
+		{name: "mardi", full_name: "Mars Descent Imager"},
+		{name: "navcam", full_name: "Navigation Camera"}
+	],
+	opportunity: [
+		{name: "fhaz", full_name: "Front Hazard Avoidance Camera"},
+		{name: "rhaz", full_name: "Rear Hazard Avoidance Camera"}, 
+		{name: "navcam", full_name: "Navigation Camera"},
+		{name: "pancam", full_name: "Panoramic Camera"},
+		{name: "minites", full_name: "Miniature Thermal Emission Sepectrometer"}
+	],
+	spirit: [
+		{name: "fhaz", full_name: "Front Hazard Avoidance Camera"},
+		{name: "rhaz", full_name: "Rear Hazard Avoidance Camera"}, 
+		{name: "navcam", full_name: "Navigation Camera"},
+		{name: "pancam", full_name: "Panoramic Camera"},
+		{name: "minites", full_name: "Miniature Thermal Emission Sepectrometer"}
+	]
+}
+
+function changeCameras(value) {
+	if (value.length == 0) {
+		document.getElementById("camera").innerHTML = "<option></option>";
+	} else {
+		var cameraOptions = "";
+		for (camera_id in rovers_by_cameras[value]) {
+			cameraOptions += "<option value='" + rovers_by_cameras[value][camera_id].name + 
+			"' id='user_camera_name'>"+ rovers_by_cameras[value][camera_id].full_name + "</option>"
+		}
+		document.getElementById("camera").innerHTML = cameraOptions;
+	}
+}
+
 function getUserChoice() {
 	var rover = document.getElementById("user_rover_name").value;
 	var camera = document.getElementById("user_camera_name").value;
-	var sol = document.getElementById("user_sol").value;
 
 	fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?sol=" + sol + "&camera=" + camera + "&api_key=DEMO_KE")
 	.then(response => {
