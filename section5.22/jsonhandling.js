86e7513caef6d98ee783ee5e3698b21935ab464e
Katherine Oh <ohkathy@gmail.com>
2018-02-17 15:09:32 -0800

    add clearPage()

--- section5.22/jsonhandling.js
+++ section5.22/jsonhandling.js
@@ -23,66 +23,84 @@ function usernameEntered() {
 	}
 }
 
+function clearPage() {
+  document.getElementById("pic").setAttribute("src", "");
+  document.getElementById("name").innerText = "";
+  document.getElementById("username").innerText = "";
+  document.getElementById("fill-icon").setAttribute("d", "");
+  document.getElementById("location").innerText = "";
+  document.getElementById("followers").innerText = "";
+  document.getElementById("followers_avatar").innerText = "";
+  document.getElementById("following").innerText = "";
+  document.getElementById("following_avatar").innerText = "";
+}
+
 function getUsername() {
 
-	// Check username is entered
 	usernameEntered();
 
 	if (isValid == true) {
     // Show Loading Gif
     document.getElementById("loader").style = "display: inline";
+
+    clearPage();
 	 
 		fetch("https://api.github.com/users/" + input_username)
 		.then(function(response_obj) {
 			return response_obj.json();
 		})
 		.then(function(obj) {		
-    
-      //Hide Loading Gif
-      document.getElementById("loader").style = "display: none";
-			
-      document.getElementById("pic").src = obj.avatar_url;
-			document.getElementById("a_pic").setAttribute("href", "http://www.github.com/" + obj.login);
-			document.getElementById("username").innerText = "@" +obj.login;
-			
-			if (obj.name !== null) {
-				document.getElementById("name").innerText = obj.name;
-			} else {
-				document.getElementById("name").innerText = "A user has no name";
-			}
+        if (obj.message == "Not Found") {
+        //Hide Loading Gif
+        document.getElementById("loader").style = "display: none";
+        document.getElementById("warning").innerText = "Sorry, user not found. Try again.";
+      } else {
+        //Hide Loading Gif
+        document.getElementById("loader").style = "display: none";
+  			
+        document.getElementById("pic").src = obj.avatar_url;
+  			document.getElementById("a_pic").setAttribute("href", "http://www.github.com/" + obj.login);
+  			document.getElementById("username").innerText = "@" +obj.login;
+  			
+  			if (obj.name !== null) {
+  				document.getElementById("name").innerText = obj.name;
+  			} else {
+  				document.getElementById("name").innerText = "A user has no name";
+  			}
 
-			// creates location icon
-			document.getElementById("fill-icon").setAttribute("d", "M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z");
+  			// creates location icon
+  			document.getElementById("fill-icon").setAttribute("d", "M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z");
 
-			if (obj.location !== null) {
-				document.getElementById("location").innerText = obj.location;
-			} else {
-				document.getElementById("location").innerText =  "Space";
-			}
-			
-			if (obj.followers > 1) {
-				document.getElementById("followers").innerText = obj.followers + " followers";
-			} else if (obj.followers == 1) {
-				document.getElementById("followers").innerText = obj.followers + " follower";
-			} else if (obj.followers == 0) {
-				document.getElementById("followers").innerText = "No followers";
-			}
-			
-			if (obj.following > 1) {
-				document.getElementById("following").innerText = "Following " + obj.following + " users";
-			} else if (obj.following == 1) {
-				document.getElementById("following").innerText = "Following " + obj.following + " user";
-			} else if (obj.following == 0) {
-				document.getElementById("following").innerText = "Not following any users";
-			}
+  			if (obj.location !== null) {
+  				document.getElementById("location").innerText = obj.location;
+  			} else {
+  				document.getElementById("location").innerText =  "Space";
+  			}
+  			
+  			if (obj.followers > 1) {
+  				document.getElementById("followers").innerText = obj.followers + " followers";
+  			} else if (obj.followers == 1) {
+  				document.getElementById("followers").innerText = obj.followers + " follower";
+  			} else if (obj.followers == 0) {
+  				document.getElementById("followers").innerText = "No followers";
+  			}
+  			
+  			if (obj.following > 1) {
+  				document.getElementById("following").innerText = "Following " + obj.following + " users";
+  			} else if (obj.following == 1) {
+  				document.getElementById("following").innerText = "Following " + obj.following + " user";
+  			} else if (obj.following == 0) {
+  				document.getElementById("following").innerText = "Not following any users";
+  			}
 
-			getFollowers(obj.followers_url);
-		  showFollowers(obj.followers_url);
-			
-			var  url = obj.following_url;
-			var trim_url = url.replace("{/other_user}", "");
-			getFollowing(trim_url);
-			showFollowing(trim_url);
+  			getFollowers(obj.followers_url);
+  		  showFollowers(obj.followers_url);
+  			
+  			var  url = obj.following_url;
+  			var trim_url = url.replace("{/other_user}", "");
+  			getFollowing(trim_url);
+  			showFollowing(trim_url);
+      }
 		
 		})
 	}
